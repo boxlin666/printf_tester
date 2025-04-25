4ec3ca62cd235ae3222ca3c69e38034023984dee
Hezi Lin <helin@bess-f2r2s18.clusters.42paris.fr>
2025-04-25 16:18:45 +0200

    first commit

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/helin/code/ftprintf
-# Build directory: /home/helin/code/ftprintf/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Tests "/home/helin/code/ftprintf/build/run_tests")
-set_tests_properties(Tests PROPERTIES  _BACKTRACE_TRIPLES "/home/helin/code/ftprintf/CMakeLists.txt;25;add_test;/home/helin/code/ftprintf/CMakeLists.txt;0;")
